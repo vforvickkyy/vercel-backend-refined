d7b8aa0b77577bd61ec4ad6b1fb320f9b0789fe6
vforvickkyy <vforvickkyy@gmail.com>
2026-02-25 00:46:00 +0530

    Token.js changed

--- api/share/[token].js
+++ api/share/[token].js
@@ -1,13 +1,9 @@
-import supabase from "../../lib/supabase";
+const supabase = require("../lib/supabase").default;
 
-export const config = {
-  runtime: "nodejs",
-};
-
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
   if (req.method === "OPTIONS") {
     return res.status(200).end();
@@ -32,6 +28,7 @@ export default async function handler(req, res) {
 
     return res.status(200).json(data);
   } catch (err) {
-    return res.status(500).json({ error: err.message });
+    console.error("SHARE ROUTE ERROR:", err);
+    return res.status(500).json({ error: "Server error" });
   }
-}
+};
